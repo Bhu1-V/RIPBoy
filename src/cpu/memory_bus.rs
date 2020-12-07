c524ab2e9335299f2a73b01372908e7a344eb447
pWNn1sher <70578657+pWNn1sher@users.noreply.github.com>
2020-12-07 10:46:08 +0530

    made a basic write byte function

--- src/cpu/memory_bus.rs
+++ src/cpu/memory_bus.rs
@@ -89,8 +89,51 @@ impl MemoryBus {
 
     pub fn write_bytes(&mut self, address: u16, value: u8) {
         let address = address as usize;
+
         match address {
-            VRAM_BEGIN..=VRAM_END => self.gpu.write_vram(address - VRAM_BEGIN, value),
+
+            BIOS_BEGIN..=BIOS_END => {
+
+                if self._inbios {
+                    if address < 0x0100 {
+                        self._bios[address - BIOS_BEGIN] = value;
+                    }
+                }
+
+                self._rom[address - ROM_0_BEGIN] = value;
+            }
+
+            ROM_0_BEGIN..=ROM_0_END => {
+                self._rom[address - ROM_0_BEGIN] = value;
+            }
+
+            ROM_1_BEGIN..=ROM_1_END => {
+                self._rom[ROM_0_SIZE + (address - ROM_1_BEGIN) ] = value;
+            }
+
+            VRAM_BEGIN..=VRAM_END => {
+
+                self.gpu.write_vram(address - VRAM_BEGIN,value);
+
+            }
+
+            EXTERNAL_RAM_BEGIN..=EXTERNAL_RAM_END => {
+                self._eram[address - EXTERNAL_RAM_BEGIN] = value;
+            }
+
+            WORKING_RAM_BEGIN..=WORKING_RAM_END | W_SHADOW_RAM_BEGIN..=W_SHADOW_RAM_END => {
+                self._wram[address - WORKING_RAM_BEGIN] = value;
+            }
+
+            SPRITE_RAM_BEGIN..=SPRITE_RAM_END => {
+                // HANDLE CORRECTLY.
+                //self.gpu.read_vram(address - VRAM_BEGIN);
+            }
+
+            ZRAM_BEGIN..=ZRAM_END => {
+                self._zram[address - ZRAM_BEGIN] = value;
+            }
+        
             _ => self.memory[address as usize] = value,
         }
     }

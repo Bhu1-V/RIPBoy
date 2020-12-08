85da0a6e43c71c94485c2e0a04d1e4b8466a9348
pWNn1sher <70578657+pWNn1sher@users.noreply.github.com>
2020-12-08 20:48:17 +0530

    added rom banking feature

--- src/cpu/memory_bus.rs
+++ src/cpu/memory_bus.rs
@@ -5,21 +5,33 @@ use super::memory_map::*;
 use crate::gpu::*;
 
 use super::CPU;
-pub struct MemoryBus {
 
+pub const MAX_CATRIDGE_SIZE: usize = 0x200000;
+pub struct MemoryBus {
     // bios flag
-    _inbios : bool,
+    _inbios: bool,
+
+    // Catridge
+    _cartridge: [u8; MAX_CATRIDGE_SIZE],
+    _mbc1: bool,
+    _mbc2: bool,
+    _current_rom_bank: u8,
+
+    _ram_banks: [u8; 0x8000],
+    _current_ram_bank: u8,
+
+    _enable_ram: bool,
+    _rom_banking: bool,
 
     // Memory Regions
-    _bios : [u8; BIOS_SIZE],
-    _rom  : [u8 ; ROM_0_SIZE],
-    _wram : [u8 ; WORKING_RAM_SIZE],
-    _eram : [u8 ; EXTERNAL_RAM_SIZE],
-    _zram : [u8 ; ZRAM_SIZE],
+    _bios: [u8; BIOS_SIZE],
+    _rom: [u8; ROM_0_SIZE],
+    _wram: [u8; WORKING_RAM_SIZE],
+    _eram: [u8; EXTERNAL_RAM_SIZE],
+    _zram: [u8; ZRAM_SIZE],
 
     pub memory: [u8; 0xFFFF],
     pub gpu: GPU,
-
 }
 
 impl fmt::Debug for MemoryBus {
@@ -31,19 +43,114 @@ impl fmt::Debug for MemoryBus {
 }
 
 impl MemoryBus {
+    pub fn load_catridge(&mut self) {
+        // Implement correctly
+        // first load catridge
+
+        match self._cartridge[0x147] {
+            1 | 2 | 3 => self._mbc1 = true,
+            5 | 6 => self._mbc2 = true,
+
+            _ => {
+                self._mbc1 = false;
+                self._mbc2 = false;
+            }
+        }
+
+        self._current_rom_bank = 1;
+        self._current_ram_bank = 0
+    }
+
+    pub fn handle_banking(&mut self, address: usize, value: u8) {
+        if address < 0x2000 {
+            if self._mbc1 || self._mbc2 {
+                self.do_ram_bank_enable(address, value);
+            }
+        } else if (address >= 0x200) && (address < 0x4000) {
+            if self._mbc1 || self._mbc2 {
+                self.do_change_lo_rom_bank(value);
+            }
+        } else if (address >= 0x4000) && (address < 0x6000) {
+            if self._mbc1 {
+                if self._rom_banking {
+                    self.do_change_hi_rom_bank(value);
+                } else {
+                    self.do_ram_bank_change(value);
+                }
+            }
+        } else if (address >= 0x6000) && (address < 0x8000) {
+            if self._mbc1 {
+                self.do_change_rom_ram_mode(value);
+            }
+        }
+    }
+
+    pub fn do_ram_bank_enable(&mut self, address: usize, value: u8) {
+        if self._mbc2 {
+            if (address & 0b_0001_0000) >> 4 == 1 {
+                return;
+            }
+        }
+
+        let test_val = value & 0xF;
+
+        if test_val == 0xA {
+            self._enable_ram = true;
+        } else if test_val == 0x0 {
+            self._enable_ram = false;
+        }
+    }
+
+    pub fn do_change_lo_rom_bank(&mut self, value: u8) {
+        if self._mbc2 {
+            self._current_rom_bank = value & 0xF;
+            if self._current_rom_bank == 0 {
+                self._current_rom_bank += 1;
+            }
+            return;
+        }
+
+        let lower5 = value & 31;
+        self._current_rom_bank &= 224; // turning off lower 5 bits
+        self._current_rom_bank |= lower5;
+
+        if self._current_rom_bank == 0 {
+            self._current_rom_bank += 1;
+        }
+    }
+
+    pub fn do_change_hi_rom_bank(&mut self, mut value: u8) {
+        self._current_rom_bank &= 31;
+
+        value &= 224;
+        self._current_rom_bank != value;
+        if self._current_rom_bank == 0 {
+            self._current_rom_bank += 1;
+        }
+    }
+
+    pub fn do_ram_bank_change(&mut self, value: u8) {
+        self._current_ram_bank = value & 0x3;
+    }
+
+    pub fn do_change_rom_ram_mode(&mut self, value: u8) {
+        let new_data = value & 0x1;
+        self._rom_banking = if new_data == 0 { true } else { false };
+
+        if self._rom_banking {
+            self._current_ram_bank = 0;
+        }
+    }
 
     pub fn read_byte(&mut self, address: u16) -> u8 {
-
         let address = address as usize;
 
         match address {
-
             BIOS_BEGIN..=BIOS_END => {
-
                 if self._inbios {
                     if address < 0x0100 {
                         return self._bios[address - BIOS_BEGIN];
-                    }else if address == 0x0100 {
+                    } else if address == 0x0100 {
                         self._inbios = false;
                     }
                 }
@@ -56,17 +163,17 @@ impl MemoryBus {
             }
 
             ROM_1_BEGIN..=ROM_1_END => {
-                return self._rom[ROM_0_SIZE + (address - ROM_1_BEGIN) ]
+                return self._cartridge
+                    [address - ROM_1_BEGIN + (self._current_rom_bank as usize * 0x4000)];
             }
 
             VRAM_BEGIN..=VRAM_END => {
-
                 return self.gpu.read_vram(address - VRAM_BEGIN);
-
             }
 
             EXTERNAL_RAM_BEGIN..=EXTERNAL_RAM_END => {
-                return self._eram[address - EXTERNAL_RAM_BEGIN];
+                return self._ram_banks
+                    [(address - 0xA000) + (self._current_ram_bank as usize * 0x2000)];
             }
 
             WORKING_RAM_BEGIN..=WORKING_RAM_END | W_SHADOW_RAM_BEGIN..=W_SHADOW_RAM_END => {
@@ -81,44 +188,33 @@ impl MemoryBus {
             ZRAM_BEGIN..=ZRAM_END => {
                 return self._zram[address - ZRAM_BEGIN];
             }
-        
+
             _ => self.memory[address as usize],
         }
-
     }
 
     pub fn write_bytes(&mut self, address: u16, value: u8) {
         let address = address as usize;
 
         match address {
-
             BIOS_BEGIN..=BIOS_END => {
-
-                if self._inbios {
-                    if address < 0x0100 {
-                        self._bios[address - BIOS_BEGIN] = value;
-                    }
-                }
-
-                self._rom[address - ROM_0_BEGIN] = value;
+                // NO WRITING TO BIOS
             }
 
-            ROM_0_BEGIN..=ROM_0_END => {
-                self._rom[address - ROM_0_BEGIN] = value;
-            }
-
-            ROM_1_BEGIN..=ROM_1_END => {
-                self._rom[ROM_0_SIZE + (address - ROM_1_BEGIN) ] = value;
+            ROM_0_BEGIN..=ROM_1_END => {
+                // NO WRITING to ROM\
+                self.handle_banking(address, value);
             }
 
             VRAM_BEGIN..=VRAM_END => {
-
-                self.gpu.write_vram(address - VRAM_BEGIN,value);
-
+                self.gpu.write_vram(address - VRAM_BEGIN, value);
             }
 
             EXTERNAL_RAM_BEGIN..=EXTERNAL_RAM_END => {
-                self._eram[address - EXTERNAL_RAM_BEGIN] = value;
+                if self._enable_ram {
+                    self._ram_banks
+                        [(address - 0xA000) + self._current_ram_bank as usize * 0x2000] = value;
+                }
             }
 
             WORKING_RAM_BEGIN..=WORKING_RAM_END | W_SHADOW_RAM_BEGIN..=W_SHADOW_RAM_END => {
@@ -133,9 +229,11 @@ impl MemoryBus {
             ZRAM_BEGIN..=ZRAM_END => {
                 self._zram[address - ZRAM_BEGIN] = value;
             }
-        
-            _ => self.memory[address as usize] = value,
+
+            _ => {
+                // unhandled call
+                println!("UNHANDLED MEM_WRITE CALL");
+            }
         }
     }
-
 }

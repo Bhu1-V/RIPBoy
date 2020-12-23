4c4aa88fc04fb50ae6582e2d6095acdb7eab8649
pWNn1sher <70578657+pWNn1sher@users.noreply.github.com>
2020-12-23 23:08:23 +0530

    cleaned

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,9 @@ use emulator::{self, Emulator};
 fn main() {
     let mut emu = Emulator::new();
     emu.start();
-    // let a = Arc::new(Mutex::new(emu.cpu));
 
-    // thread::spawn( move || {
-    //     let c = Arc::clone(&a);
-    //     let app = App::default();
-    //     let mut wind = Window::new(100, 100, 400, 300, "Hello from rust");
-    //     let mut frame = Frame::new(0, 0, 400, 200, "");
-    //     let mut but = Button::new(160, 210, 80, 40, "Click me!");
-    //     let data = c.lock().unwrap();
-    //     println!("Printing Registers = {:?}",data.registers);
-    //     wind.end();
-    //     wind.show();
-    //     but.set_callback(move || frame.set_label("Hello World!"));
-    //     app.run().unwrap();
-    //     thread::sleep(Duration::from_millis(1));
-    // });
-
-    // let new_cpu_arc = Arc::clone(&a);
     loop {
         emu.emulate();
-        // thread::sleep(Duration::from_millis(2));
     }
 
 
